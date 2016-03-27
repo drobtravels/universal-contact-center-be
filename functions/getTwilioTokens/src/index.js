c610dfe8bfb98df0087a464798ebc9c145fcd004
David Roberts <drob@drob.net>
2016-03-27 13:21:31 -0400

    validateUser works as expected

--- functions/getTwilioTokens/src/index.js
+++ functions/getTwilioTokens/src/index.js
@@ -3,22 +3,36 @@ import jwt from 'jsonwebtoken';
 
 
 let validateUser = function(authToken) {
-  var secretBuf = new Buffer(process.env.AUTH_SECRET, 'base64');
-  jwt.verify(authToken, secretBuf, function(err, decoded) {
-    if(err) {
-      console.log('failed jwt verify: ', err, 'auth: ', authToken);
-      return(false);
-    } else {
-      console.log('succesfully authenticated')
-      return(decoded);
-    }
-  })
+  try {
+    var secretBuf = new Buffer(process.env.AUTH_SECRET, 'base64');
+    var decoded = jwt.verify(authToken, secretBuf)
+    console.log('succesfully authenticated')
+    console.log(decoded);
+    return(decoded);
+  } catch(err) {
+    console.log('failed jwt verify: ', err, 'auth: ', authToken);
+    return(false);
+  }
+}
+
+let getTaskRouterToken = function() {
+  return('');
+}
+
+let getTwilioClientToken = function() {
+  return('something');
 }
 
 export default function(event, context) {
   var userDetails = validateUser(event.userToken);
-  if (userDetails) {
-    context.succeed('ok');
+  if( userDetails ) {
+    context.succeed({
+      status: 'success',
+      tokens: {
+        taskRouter: getTaskRouterToken(),
+        twilioClient: getTwilioClientToken()
+      }
+    });
   } else {
     context.fail('invalid user');
   }
