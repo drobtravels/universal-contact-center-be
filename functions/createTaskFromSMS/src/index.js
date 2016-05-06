f5d3e911113d047ad52a2c8b9d1bc6fd090f672d
David Roberts <drob@drob.net>
2016-05-05 23:03:09 -0400

    Can create tasks from Twilio SMS Messages

---
+++ functions/createTaskFromSMS/src/index.js
@@ -0,0 +1,15 @@
+import qs from 'qs';
+import twilio from 'twilio';
+let twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+let twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+let taskRouterWorkspaceSid = process.env.TASK_ROUTER_WORKSPACE_SID;
+
+
+export default function(event, context) {
+  var params = qs.parse(event.encodedParams)
+  params.type = 'sms_message'
+  var client = new twilio.TaskRouterClient(twilioAccountSid, twilioAuthToken, taskRouterWorkspaceSid);
+  client.workspace.tasks.create({ attributes: JSON.stringify(params) }).then( (task) => {
+    context.succeed({});
+  }, (error) => { context.fail(error) });
+}
