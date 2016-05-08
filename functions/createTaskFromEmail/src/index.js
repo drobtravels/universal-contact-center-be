5f15ce7c5c377c27ba38515fa69843f46ab2afd2
David Roberts <drob@drob.net>
2016-05-08 15:28:18 -0400

    Can create tasks from e-mails using Postmark.  This was much simpler because they send JSON requests

--- functions/createTaskFromEmail/src/index.js
+++ functions/createTaskFromEmail/src/index.js
@@ -3,17 +3,18 @@ let twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 let twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 let taskRouterWorkspaceSid = process.env.TASK_ROUTER_WORKSPACE_SID;
 
-
 export default function(event, context) {
-  console.log(event);
-  var email_info = event.message;
   var params = {
     type: 'email',
-    From: email_info.from,
-    Body: email_info.text,
-    Subject: email_info.subject,
-    To: email_info.to
-  };
+    fromEmail: event.FromEmail,
+    name: event.FromName,
+    message: event.Message,
+    subject: event.Subject,
+    messageID: event.MessageID,
+    date: event.Date
+  }
+
+  console.log('create task with params', params)
   var client = new twilio.TaskRouterClient(twilioAccountSid, twilioAuthToken, taskRouterWorkspaceSid);
   client.workspace.tasks.create({ attributes: JSON.stringify(params) }).then( (task) => {
     context.succeed({});
